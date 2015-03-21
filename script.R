1699622c812b972b4dea6b92522c7fd57d7080fe
hfoffani <hfoffani@gmail.com>
2015-03-21 18:18:42 +0100

    Trying things

--- script.R
+++ script.R
@@ -4,3 +4,13 @@
 # 3. count words.
 # 4. histograms.
 
+wrd <- function (f) {
+fname <- paste("en_US.", f, ".txt", sep="")
+corpus <- load.corpus(fname, corpus.dir = "data")
+words <- txt.to.words.ext(corpus, language = "English.contr")
+# ng2 <- make.ngrams(words, ngram.size=2)
+# ng3 <- make.ngrams(words, ngram.size=3)
+# fwords <- as.data.frame(table(words), stringsAsFactors=F)
+return (words)
+}
+
