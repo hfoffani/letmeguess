85c77a60dbd59fcaff0fb7bbf36345d06811528c
Hernan Foffani <hfoffani@gmail.com>
2015-03-27 12:37:43 +0100

    QUIZ 2

---
+++ data/q2-n3.sh
@@ -0,0 +1,88 @@
+
+N=3
+
+echo QUIZ 2
+echo
+echo
+echo Q1
+grep "case of cheese" freq.$N.*.txt
+grep "case of soda" freq.$N.*.txt
+grep "case of pretzels" freq.$N.*.txt
+grep "case of beer" freq.$N.*.txt
+
+
+echo
+echo
+echo Q2
+grep "mean the most" freq.$N.*.txt
+grep "mean the best" freq.$N.*.txt
+grep "mean the universe" freq.$N.*.txt
+grep "mean the world" freq.$N.*.txt
+
+
+echo
+echo
+echo Q3
+grep "me the saddest" freq.$N.*.txt
+grep "me the smelliest" freq.$N.*.txt
+grep "me the happiest" freq.$N.*.txt
+grep "me the bluest" freq.$N.*.txt
+
+
+echo
+echo
+echo Q4
+grep "but the crowd" freq.$N.*.txt
+grep "but the referees" freq.$N.*.txt
+grep "but the players" freq.$N.*.txt
+grep "but the defense" freq.$N.*.txt
+
+
+echo
+echo Q5
+grep "at the mall" freq.$N.*.txt
+grep "at the movies" freq.$N.*.txt
+grep "at the beach" freq.$N.*.txt
+grep "at the grocery" freq.$N.*.txt
+
+
+echo
+echo Q6
+grep "on my phone" freq.$N.*.txt
+grep "on my horse" freq.$N.*.txt
+grep "on my motorcycle" freq.$N.*.txt
+grep "on my way" freq.$N.*.txt
+
+
+echo
+echo Q7
+grep "quite some years" freq.$N.*.txt
+grep "quite some thing" freq.$N.*.txt
+grep "quite some time" freq.$N.*.txt
+grep "quite some weeks" freq.$N.*.txt
+
+
+echo
+echo Q8
+grep "his little eyes" freq.$N.*.txt
+grep "his little toes" freq.$N.*.txt
+grep "his little ears" freq.$N.*.txt
+grep "his little fingers" freq.$N.*.txt
+
+
+echo
+echo Q9
+grep "during the sad" freq.$N.*.txt
+grep "during the bad" freq.$N.*.txt
+grep "during the hard" freq.$N.*.txt
+grep "during the worse" freq.$N.*.txt
+
+
+echo
+echo Q10
+grep "must be asleep" freq.$N.*.txt
+grep "must be insensitive" freq.$N.*.txt
+grep "must be insane" freq.$N.*.txt
+grep "must be callous" freq.$N.*.txt
+
+
