a6cc3dfec165b3e8bab5ded6ff6861af99045def
hfoffani <hfoffani@gmail.com>
2015-04-04 22:02:40 +0200

    add random predictor.

---
+++ simpsons.R
@@ -0,0 +1,12 @@
+simpsons <- function() {
+    dictionary <- "http://pastebin.com/raw.php?i=anKcMdvk"
+    words <- read.table(dictionary, stringsAsFactors=F, skip=4, quote="", header=T)
+    words$Frequency <- as.numeric( gsub("\\(|\\)", "", words$Frequency) )
+    words$Probs <- words$Frequency / sum(words$Frequency)
+    return(words)
+}
+
+bartreplies <- function(dictionary) {
+    s <- sample(nrow(dictionary), 1, prob=dictionary$Probs)
+    return( dictionary$Word[s] )
+}
