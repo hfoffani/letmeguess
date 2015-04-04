8bf7463d4d779e32448a58c314167815a1f07fa0
Hernan Foffani <hfoffani@gmail.com>
2015-04-04 14:38:55 +0200

    predict!!!!

--- script.R
+++ script.R
@@ -4,19 +4,19 @@
 # 3. hash 2-gram
 # 4. from 2-gram to dict of lists of pairs
 
-# di <- read.dict()
-# n1 <- read.ngram(1)
-# n2 <- read.ngram(2)
-# n3 <- read.ngram(3)
-# n4 <- read.ngram(4)
+# di <- text.read.dict()
+# n1 <- text.read.ngram(1)
+# n2 <- text.read.ngram(2)
+# n3 <- text.read.ngram(3)
+# n4 <- text.read.ngram(4)
 
-read.dict <- function () {
+text.read.dict <- function () {
     w <- read.csv('data/di.csv', header=F, stringsAsFactors=F)
     names(w) <- c('word','id')
     return(w)
 }
 
-read.ngram <- function (n) {
+text.read.ngram <- function (n) {
     l1 <- list('y','count')
     l2 <- list('x1','y','count')
     l3 <- list('x1','x2','y','count')
@@ -29,5 +29,39 @@ read.ngram <- function (n) {
     
     names(ngr) <- unlist(ln[[n]])
     return(ngr)
+}
+
+wordid <- function( w, di ) {
+    return(match(w, di))
+}
+
+
+text.candidates <- function (n, ng, wordsids) {
+    idx <- switch ( n, {
+                NULL     
+            }, {
+                ng$x1 == wordsids[3]
+            }, {
+                ng$x1 == wordsids[2] & ng$x2 == wordsids[3]                 
+            }, { 
+                ng$x1 == wordsids[1] & ng$x2 == wordsids[2] & ng$x3 == wordsids[3]                 
+            })
+    ies <- data.frame(w=ng$y[idx], c=ng$count[idx])
+    return(ies)
+}
+
+text.predict <- function (wordsids) {
+    weights <- c(0.25, 0.25, 0.25, 0.25)
+    c2 <- text.candidates(2, n2, wordsids)
+    c3 <- text.candidates(3, n3, wordsids)
+    c4 <- text.candidates(4, n4, wordsids)
+    m <- merge(c2, c3, by="w", all=T)
+    m <- merge(m, c4, by="w", all=T)
+    names(m) <- c('w','c2','c3','c4')
+    m$c1 <- n1[n1$y %in% m$w,]$count
+    w <- m$w
+    m$w <- NULL
+    m <- data.frame(mapply(`*`,m, weights))
+    return( w[which.max(rowSums(m, na.rm=T))] )
 }
 
