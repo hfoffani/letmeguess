d961a8fecb97b5cf5f5d495ba938bfcf82b93288
hfoffani <hfoffani@gmail.com>
2015-04-05 17:43:16 +0200

    Using data.table for increased performance.

--- script.R
+++ script.R
@@ -1,4 +1,4 @@
-
+library(data.table)
 
 text.read.dict <- function () {
     w <- read.csv('data/di.csv', header=F, stringsAsFactors=F)
@@ -20,7 +20,17 @@ text.read.ngram <- function (n) {
     names(ngr) <- unlist(ln[[n]])
     tot <- sum(ngr$count)
     ngr$count <- ngr$count / tot
-    return(ngr)
+    dfngr <- data.table(ngr)
+    switch (n, {
+        setkey(dfngr, y)
+    }, {
+        setkey(dfngr, x1) 
+    }, {
+        setkey(dfngr, x1, x2)
+    }, {
+        setkey(dfngr, x1, x2, x3)
+    })
+    return(dfngr)
 }
 
 text.wordid <- function( w, di ) {
@@ -30,38 +40,41 @@ text.wordid <- function( w, di ) {
 text.candidates <- function (n, ng, wordsids) {
     l <- length(wordsids)
     idx <- switch ( n, {
-                FALSE     
+                NULL     
             }, {
                 if (l >= 1)
-                    ng$x1 == wordsids[l]
+                    ng[J(wordsids[l]),
+                        c('y','count'),with=F]
                 else
-                    FALSE
+                    data.table(y=integer(0), count=numeric(0))
             }, {
                 if (l >= 2)
-                    ng$x1 == wordsids[l-1] & ng$x2 == wordsids[l]
+                    ng[J(wordsids[l-1],wordsids[l]),
+                        c('y','count'),with=F]
                 else
-                    FALSE
+                    data.table(y=integer(0), count=numeric(0))
             }, {
                 if (l >= 3)
-                    ng$x1 == wordsids[l-2] & ng$x2 == wordsids[l-1] &
-                    ng$x3 == wordsids[l]
-                else FALSE
+                    ng[J(wordsids[l-2],wordsids[l-1],wordsids[l]),c('y','count'),with=F]
+                else
+                    data.table(y=integer(0), count=numeric(0))
             })
-    ies <- data.frame(w=ng$y[idx], c=ng$count[idx])
-    return(ies)
+    # ies <- data.frame(w=idx$y, c=idx$count)
+    return(idx)
 }
 
 text.guessword <- function (wordsids, n1, n2, n3, n4, weights=NULL) {
     c2 <- text.candidates(2, n2, wordsids)
     c3 <- text.candidates(3, n3, wordsids)
     c4 <- text.candidates(4, n4, wordsids)
-    m <- merge(c2, c3, by="w", all=T)
-    m <- merge(m, c4, by="w", all=T)
-    names(m) <- c('w','c2','c3','c4')
-    m$c1 <- n1[n1$y %in% m$w,]$count
+    m <- merge(c2, c3, by="y", all=T, suffixes=c(".c2",".c3"))
+    m <- merge(m, c4, by="y", all=T)
+    m <- merge(m, n1, by="y", suffixes=c(".c4",".c1"))
+    setnames(m, 1:5, c('w','c2','c3','c4','c1'))
+    # m$c1 <- n1[n1$y %in% m$w,]$count
     w <- m$w
-    m$w <- NULL
-    m <- m[c("c1","c2","c3","c4")]     
+    # m$w <- NULL
+    m <- m[,c("c1","c2","c3","c4"),with=F]     
     # weight.
     if (is.null(weights))
         weights<-c(0.0001, 1, 5, 10)
