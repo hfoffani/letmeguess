347ed6b138bf6a4769a25e3c8dbab386fc733b6a
Hernan Foffani <hfoffani@gmail.com>
2015-04-14 16:22:01 +0200

    First roundup of text input -> output word.

---
+++ shinysimpsons/server.R
@@ -0,0 +1,16 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+# 
+# http://www.rstudio.com/shiny/
+#
+
+library(shiny)
+
+shinyServer(function(input, output) {
+   
+  output$predictedWord <- renderText({
+      # 'hola'
+      input$userText
+      })
+})
